2042b4073da596f886a33efe35cb8dfa11ea1ad5
stargazing4122 <jadenyeiden@gmail.com>
2022-02-04 14:09:28 -0500

    🏠something of view at home added

--- src/components/home/HomeScreen.tsx
+++ src/components/home/HomeScreen.tsx
@@ -1,9 +1,24 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 
 const HomeScreen = () => {
+  const history = useHistory();
+  const handleClickGo = () => {
+    history.push('/todos');
+  };
   return (
-        <h1>Hi from Home Screen</h1>
-    );
+    <>
+      <h1>Home</h1>
+      <p>Hi click in the button below to start.</p>
+      <button
+        className="btn btn-outline-info"
+        type="button"
+        onClick={handleClickGo}
+      >
+        Go
+      </button>
+    </>
+  );
 };
 
 export default HomeScreen;
