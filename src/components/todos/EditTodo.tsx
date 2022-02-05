544fe14b5fab2c026701a629d0d7b0e6c87adb0f
stargazing4122 <jadenyeiden@gmail.com>
2022-02-04 22:14:03 -0500

    🖋️ logic of edit todo added

---
+++ src/components/todos/EditTodo.tsx
@@ -0,0 +1,61 @@
+import React, { FormEvent } from 'react';
+import { TodoAction, doEditTodo } from '../../actions/TodosActions';
+import useForm from '../../hooks/useForm';
+import { Todo } from '../../interfaces/interfaces';
+
+interface EditTodoProps {
+  todo: Todo;
+  setEditionMode: any;
+  todosDispatch: (action: TodoAction) => void;
+}
+
+interface FormData {
+  newTitle: string;
+}
+const EditTodo = ({ todo, setEditionMode, todosDispatch }: EditTodoProps) => {
+  const { formValues, handleValue } = useForm<FormData>({
+    newTitle: '',
+  });
+  const { newTitle } = formValues;
+
+  const handleCancel = (): void => {
+    setEditionMode(false);
+  };
+
+  const handleEditTodo = (e: FormEvent<HTMLFormElement>): void => {
+    e.preventDefault();
+    const todoEdited: Todo = {
+      ...todo,
+      title: newTitle,
+    };
+    todosDispatch(doEditTodo(todoEdited));
+  };
+  return (
+    <form onSubmit={handleEditTodo}>
+      <input
+        style={{
+          width: '106px',
+          display: 'inline',
+        }}
+        className="form-control me-1"
+        type="text"
+        placeholder={todo.title}
+        value={newTitle}
+        name="newTitle"
+        onChange={handleValue}
+      />
+      <button type="submit" className="btn btn-outline-info me-1">
+        Save
+      </button>
+      <button
+        type="button"
+        className="btn btn-outline-danger"
+        onClick={handleCancel}
+      >
+        X
+      </button>
+    </form>
+  );
+};
+
+export default EditTodo;
--- src/components/todos/ListTodosItem.tsx
+++ src/components/todos/ListTodosItem.tsx
@@ -1,6 +1,7 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import TodoContext from '../../context/TodoContext';
 import { doDeleteTodo, doToggleTodo } from '../../actions/TodosActions';
+import EditTodo from './EditTodo';
 
 interface TodosItemProps {
   id: string;
@@ -10,6 +11,8 @@ interface TodosItemProps {
 }
 
 const ListTodosItem = ({ index, id, title, state }: TodosItemProps) => {
+  const [editionMode, setEditionMode] = useState(false);
+
   const { todosDispatch } = useContext(TodoContext);
 
   const handleClickDelete = (id: string): void => {
@@ -19,40 +22,60 @@ const ListTodosItem = ({ index, id, title, state }: TodosItemProps) => {
   const handleClickToggle = (id: string): void => {
     todosDispatch(doToggleTodo(id));
   };
+
+  const handleSetEditionMode = (): void => {
+    setEditionMode(true);
+  };
   return (
-    <tr>
-      <td>{index + 1}</td>
-      <td>{id}</td>
-      <td
-        onClick={() => handleClickToggle(id)}
-        style={{
-          cursor: 'pointer',
-          textDecoration: state ? 'line-through' : '',
-        }}
-      >
-        {title}
-      </td>
-      <td>{state ? 'completed' : 'incompleted'}</td>
-      <td>
-        <button
-          type="button"
-          className="btn btn-success me-1"
+    <>
+      <tr>
+        <td>{index + 1}</td>
+        <td>{id}</td>
+        <td
           onClick={() => handleClickToggle(id)}
+          style={{
+            cursor: 'pointer',
+            textDecoration: state ? 'line-through' : '',
+          }}
         >
-          Toggle
-        </button>
-        <button type="button" className="btn btn-info me-1">
-          Edit
-        </button>
-        <button
-          type="button"
-          className="btn btn-danger me-1"
-          onClick={() => handleClickDelete(id)}
-        >
-          Delete
-        </button>
-      </td>
-    </tr>
+          {title}
+        </td>
+        <td>{state ? 'completed' : 'incompleted'}</td>
+        <td>
+          {!editionMode ? (
+            <>
+              <button
+                type="button"
+                className="btn btn-success me-1"
+                onClick={() => handleClickToggle(id)}
+              >
+                Toggle
+              </button>
+              <button
+                type="button"
+                className="btn btn-danger me-1"
+                onClick={() => handleClickDelete(id)}
+              >
+                Delete
+              </button>
+              <button
+                type="button"
+                className="btn btn-info me-1"
+                onClick={handleSetEditionMode}
+              >
+                Edit
+              </button>
+            </>
+          ) : (
+            <EditTodo
+              todo={{ id, title, state }}
+              setEditionMode={setEditionMode}
+              todosDispatch={todosDispatch}
+            />
+          )}
+        </td>
+      </tr>
+    </>
   );
 };
 
